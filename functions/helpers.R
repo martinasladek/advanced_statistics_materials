66036c698b730687bed4f23d030fd26042691cde
martinasladek <m.sladekova@sussex.ac.uk>
2026-01-28 12:54:47 +0000

    alert fix

--- functions/helpers.R
+++ functions/helpers.R
@@ -1,3 +1,7 @@
 squish <- function(x, new_min = min_bound, new_max = max_bound){   
   (x - min(x))/(max(x)-min(x)) * (new_max - new_min) + new_min 
 }
+
+rqpois <- function(n, mu, theta) {
+  rnbinom(n = n, mu = mu, size = mu/(theta-1))
+}
