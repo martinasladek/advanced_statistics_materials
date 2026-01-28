d5f8a7ac9b87a195c301940ebfef208c4553f3a0
martinasladek <m.sladekova@sussex.ac.uk>
2026-01-28 19:17:21 +0000

    publish counts draft.

--- functions/helpers.R
+++ functions/helpers.R
@@ -5,3 +5,30 @@ squish <- function(x, new_min = min_bound, new_max = max_bound){
 rqpois <- function(n, mu, theta) {
   rnbinom(n = n, mu = mu, size = mu/(theta-1))
 }
+
+report_pseudo_r2 <- function(glm){
+  
+  perf = performance(glm)
+  
+  list(
+    percent = (perf$R2_Nagelkerke*100) |> round(2) |> paste0("%"),
+    r2 = paste0("*R^2^* = ", round(perf$R2_Nagelkerke, 2))
+  )
+  
+}
+
+report_wald <- function(lm){
+  
+  lm_wald <- test_wald(lm) |> suppressMessages()
+  
+  f = lm_wald$F[2] |> round(2)
+  df_1 = lm_wald$df_diff[2] |> round(2)
+  df_2 = lm_wald$df[2] |> round(2)
+  wald_p <- lm_wald$p[2]
+  report_p = ifelse(wald_p < 0.001, "*p* < .001", paste0("*p* = ", wald_p))
+  
+  paste0(
+    "*F*(", df_1,  ",", df_2, ") = ", f,  ", ", report_p  
+  )
+  
+}
